ba5f0fe7f5dc1895d68d80f655a166fe2d13066e
Ehevi <aga.cyra@wp.pl>
2022-03-01 14:46:56 +0100

    mocks

---
+++ mocks/mockHttpsConn/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+bash make-root-ca-and-certificates.sh 'localhost'
+echo ""
+
+echo ""
+kill -9 `lsof -t -i:8043`
+node serve.js 8043 &
+NODE_PID=$!
+sleep 1
+
+echo ""
+echo ""
+node request.js 8043 'localhost'
+echo -n " - without warnings, love node.js' https"
+echo ""
+sleep 1
+
+echo ""
+curl https://localhost:8043 \
+  --cacert certs/client/chain.pem
+echo -n " - without warnings, love cURL"
+echo ""
+sleep 1
+
+kill ${NODE_PID}
+echo ""
